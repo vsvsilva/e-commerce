bf40914003c22499b5f34ed995fa336337a8e4ab
Vitor <vitor_ovitor@outlook.com>
2021-10-18 10:40:03 -0300

    Mock Products

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Products from './components/products/Products'
 
 const App = () => {
     return (
         <div>
-            E-Commerce
+            <Products />
         </div>
     )
 }
