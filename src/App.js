27d78327a86f4cfa696ea875bc37c182eb2afcb8
Vitor <vitor_ovitor@outlook.com>
2021-10-22 19:27:20 -0300

    cart-layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import {Products, Navbar } from './components'
+import {Products, Navbar, Cart } from './components'
 import { commerce } from './lib/Commerce'
 
 const App = () => {
@@ -29,7 +29,8 @@ const App = () => {
     return (
         <div>
             <Navbar totalItems={cart.total_items}/>
-            <Products products={products} onAddToCart={handleAddToCart}/>
+            {/* <Products products={products} onAddToCart={handleAddToCart}/> */}
+            <Cart cart={cart}/>
         </div>
     )
 }
