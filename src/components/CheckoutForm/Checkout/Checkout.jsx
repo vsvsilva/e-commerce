37361bb370f2ea1ca7f7b4a5f0b2c7333a04ac40
Vitor <vitor_ovitor@outlook.com>
2021-12-01 14:17:42 -0300

    payment-form-&-stripe

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -7,7 +7,7 @@ import PaymentForm from '../PaymentForm'
 
 const steps = ['Shipping address', 'Payment details']
 
-const Checkout = ({ cart }) => {
+const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
     const classes = useStyles()
     const [activeStep, setActiveStep] = useState(0)
     const [checkoutToken, setCheckoutToken] = useState(null)
@@ -42,7 +42,13 @@ const Checkout = ({ cart }) => {
 
     const Form = () => activeStep === 0
         ? <AddressForm checkoutToken={checkoutToken} next={next}/>
-        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken}/>
+        : <PaymentForm
+            shippingData={shippingData}
+            checkoutToken={checkoutToken}
+            backStep={backStep}
+            nextStep={nextStep}
+            onCaptureCheckout={onCaptureCheckout}
+            />
     
     return (
         <>
